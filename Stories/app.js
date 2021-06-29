d3485336c5fabfc88af5eee89005bd75bcf811a9
Sameer Dudeja <dsameer33@gmail.com>
2021-06-30 04:54:14 +0530

    Authsetup

--- Stories/app.js
+++ Stories/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const path = require('path')
-const connectDB = require('./config/db')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const connectDB = require('./config/db')
+
 //load config
 dotenv.config({ path: './config/config.env' })
 
+//Passport Config
+require('./config/passport')(passport)
 connectDB()
 
 const app = express()
@@ -20,12 +25,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
+//session Middleware
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+//Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
 //Static folder
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.Port || 3000
 
--- Stories/routes/index.js
+++ Stories/routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-//@desc Login/Landing page
-//@ route Get /
+// @desc Login/Landing page
+// @route Get /
 router.get('/', (req, res) => {
   res.render('login', { layout: 'login' })
 })
 
+// @desc Dashboard
+// @route Get /dashboard
 router.get('/dashboard', (req, res) => {
   res.render('dashboard')
 })
