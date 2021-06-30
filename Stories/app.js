a80700d8473690b6304ab039e73284ccf07e29a3
Sameer Dudeja <dsameer33@gmail.com>
2021-07-01 03:03:52 +0530

    UserStories

--- Stories/app.js
+++ Stories/app.js
@@ -18,13 +18,24 @@ connectDB()
 
 const app = express()
 
+//body Parser
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 //logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+//Handlebar helpers
+const { formatDate } = require('./helpers/hbs')
+
 //Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine(
+  '.hbs',
+  exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' })
+)
 app.set('view engine', '.hbs')
 
 //session Middleware
@@ -40,13 +51,14 @@ app.use(
 //Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
-//Static folder
 
+//Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.Port || 3000
 
